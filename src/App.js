737a2d46b9f00be5bf6710875a174bbd679bb83f
Sage Choi <coolcuds@gmail.com>
2023-06-15 21:16:02 -0700

    Add JSX to dispaly the webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import timelineData from './data/timeline.json';
+import './components/Timeline.css';
 import Timeline from './components/Timeline';
 
 function App() {
-    console.log('The value of timelineData is', timelineData);
-    console.log('We should use timelineData in our project somehow...');
-
-    return <div></div>;
+  return (
+    <div>
+      <header className={`${'App-header'} ${'App-title'}`}>
+        Ada Lovelace's social media feed
+      </header>
+      <main className='App-main'>
+        <Timeline
+          events={timelineData.events}
+        />
+      </main>
+    </div>
+  )
 }
 
 export default App;
