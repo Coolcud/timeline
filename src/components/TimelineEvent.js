332a2053d4666e0425703c0f717a9b5395ae160c
Sage Choi <coolcuds@gmail.com>
2023-06-15 21:17:43 -0700

    Add JSX for displaying Timeline events

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,6 +1,18 @@
 import './TimelineEvent.css';
 import TimeStamp from './TimeStamp';
 
-const TimelineEvent = () => {};
+const TimelineEvent = ({ person, status, timestamp }) => {
+  return (
+    <div>
+      <section className='timeline-event'>
+        <p className='event-person'>{person}</p>
+        <p className='event-status'>{status}</p>
+        <p className='event-time'><TimeStamp time={timestamp}>
+            </TimeStamp>
+        </p>
+      </section>
+    </div>
+  );
+};
 
 export default TimelineEvent;
