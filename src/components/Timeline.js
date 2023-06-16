37d79afb1a3060db915bf6585099e8baa76a62dc
Sage Choi <coolcuds@gmail.com>
2023-06-15 21:16:58 -0700

    Map each event from events to render individually on webpage

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,6 +1,31 @@
 import './Timeline.css';
+import PropTypes from 'prop-types';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {};
+const Timeline = ({ events }) => {
+  const timelineComponents = events.map(function( event, index ) {
+    return (
+      <div key={index}>
+        <TimelineEvent
+          person={event.person}
+          status={event.status}
+          timestamp={event.timestamp}
+        />
+      </div>
+    );
+  });
+
+  return (
+    <section className='timeline'>{timelineComponents}</section>
+  );
+};
+
+Timeline.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({
+    person: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    timestamp: PropTypes.string.isRequired,
+  }))
+};
 
 export default Timeline;
